d81b14f14a58e8fbcd3095268aebe2b26e78667e
Matt Mapleston <matt@chanzachanzo.com>
2013-12-14 22:33:08 +0000

    Responsive grid layout in client page o tried stylehatch/photoset-grid (lots of stuff to install) didn't really try o grid-a-licious: ok, but not good info or column spanning images o masonry: better support, got it working eventually, cross-column imgs?? o added lots of images to the server

--- public/script.js
+++ public/script.js
@@ -1,13 +1,19 @@
 //  Functions:
-// addMessage: a helper functions used by other scripts in this file
-// sentMessage: send a message
-// setPseudo: set user's pseudonym, send it to the server, show chat controls, hide pseudo bits
-// listen to socket
+//  **Chat
+//        addMessage: a helper functions used by other scripts in this file
+//        sentMessage: send a message
+//        setPseudo: set user's pseudonym, send it to the server, show chat controls, hide pseudo bits
+//        listen to socket for message event (incoming messages from other clients
+//
+//  **Media Functions
+//        showImage(imgSrc, #divName)
+//        playSound(sound source)
 // init function
 
 
 var socket = io.connect();
 
+// **Chat functions
 function addMessage(msg, pseudo) {
   $("#chatEntries").append('<div class="message"><p>' + pseudo + ' : ' + msg + '</p></div>');
 }
@@ -35,6 +41,9 @@ socket.on('message', function(data) {
   addMessage(data['message'], data['pseudo']);
 });
 
+
+// **Mediafunctions
+
 //showImage pseudo code:
 // call a function passing an image - done.
 // get image aspect ratio (width/height)
@@ -42,7 +51,7 @@ socket.on('message', function(data) {
 // if browser is too wide, set image height to browser height and image width = height * aspect ratio
 // if browser is too thin, set image width to browser width and image height = width/aspect ratio
 // 
-function showImage(imgSrc) {
+function showImage(imgSrc, divName) {
     var img = document.createElement("img");
     browserWidth = $(window).width();
     browserHeight= $(window).height();
@@ -52,13 +61,10 @@ function showImage(imgSrc) {
     img.height = browserHeight;//$(window).height();//browserHeight;
     img.alt = "alt text";
 
-    // This next line will just add it to the <body> tag
-    //document.body.appendChild(img);
-    //$('#imageDisplay').appendChild(img);
-    //$('#imageDisplay').appendChild("img");
     //$("#imageDisplay").prepend("<img id='theImg' src='images/jum.png'/>"); 
-    $("#imageDisplay").prepend(img); 
-    console.log("showImg: " + img.src + " w: " + browserWidth + " h: " + browserHeight);
+    console.log("showImage " +img.src + " in " + divName);
+    $(divName).prepend(img); 
+    //console.log("showImg: " + img.src + " w: " + browserWidth + " h: " + browserHeight);
 }
 
 function playSound(sndSrc) {
@@ -68,17 +74,53 @@ function playSound(sndSrc) {
 //init function fired once page loaded
 //hide chat controls 2 button listeners
 $(function() {
+  var $thegrid = $("#thegrid");
   $("#chatControls").hide();
-  $("#pseudoSet").click(function(e) {setPseudo();});
-  $("#submit").click(function() {sentMessage();});
-  $("#audio").click(function() {playSound('sounds/foghorn.wav');});
-  $("#audio").click(function() {showImage('images/jum.png')});
-  $("#imageDisplay").ready(function() {showImage('images/beardygrin.jpg')});
-  //$("#imageDisplay").prepend("<img id='theImg' src='images/jum.png'/>");
-  //showImage("http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2010/5/20/1274352435003/Dog-poo-2-002.jpg");
-//  $("#imageDisplay")
-//  $("#imageDisplay").fullscreenr({width: 1229, height: 768});
-//  $.fn.showImage = function() {showImage('http://google.com/images/logo.gif', 276, 110, 'google logo alt');});
+  $("#pseudoControls").hide();
+  //$("#pseudoSet").click(function(e) {setPseudo();});
+  //$("#submit").click(function() {sentMessage();});
+  //$("#audio").click(function() {playSound('sounds/foghorn.wav');});
+  //$("#audio").click(function() {showImage('images/jum.png', '#imageDisplay')});
+  //$("#imageDisplay").ready(function() {showImage('images/beardygrin.jpg', '#imageDisplay')});
+  //$("#thegrid").ready(function() {showImage('images/beardygrin.jpg', "#thegrid")});
+  //$("thegrid").gridalicious('append', boxes)
+  
+  
+  //** The Grid
+ imagesLoaded('#thegrid', function() {
+  console.log("Got to images Loades");
+    $('#thegrid').masonry({
+      itemSelector: 'item',
+      columnWidth: 126,
+      gutter: 1
+    });
+    console.log("grid options done");
+ })
+  
+  $('#thegrid').masonry();
+  //var msnry = new Masonry( $('#thegrid'), {
+  //  itemSelector: 'item',
+  //  columnWidth: 70
+  //});
+  //var msnry = new Masonry('#thegrid');
 
+  //var thegrid = document.querySelector('#thegrid');
+  //var msnry = new Masonry( thegrid, {
+  //  columnWidth: 600,
+  //  itemSelector: '.item'
+  //});
+  
+    //$thegrid.loaded(function() {
+  //  $thegrid.masonry({
+  //    itemSelector: '.masonryImage',
+  //    columnWidth: 100
+  //  });
+  //});
+
+  //var msnry = $thegrid.data('masonry');
+  //console.log("msnry: "+ msnry);
+  
+   //.append(('<img id="theImg2" src="images/2.png"/>'))
+   //.addItems(('<img id="theImg3" src="images/3.png"/>'));
 });
 
