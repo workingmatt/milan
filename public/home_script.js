2aacbeb0637ae3a2557e46931264aed4abfc8ce1
root <root@chanzachanzo24.(none)>
2014-01-27 15:10:18 +0000

    At venue change: viewpoint solution trials on chanzachanzo24

--- public/home_script.js
+++ public/home_script.js
@@ -11,158 +11,158 @@ var browserHeight= $(window).height();
 
 
 function showImage(imgSrc, divName) {
-    var img = document.createElement("img");
-    browserWidth = $(window).width();
-    browserHeight= $(window).height();
-    
-    img.src = imgSrc;
-    img.width = browserWidth;
-    img.alt = "alt text";
+var img = document.createElement("img");
+browserWidth = $(window).width();
+browserHeight= $(window).height();
 
-    console.log("showImage " +img.src + " in " + divName);
-    $(divName).prepend(img); 
+img.src = imgSrc;
+img.width = browserWidth;
+img.alt = "alt text";
+
+console.log("showImage " +img.src + " in " + divName);
+$(divName).prepend(img); 
 }
 
 function addImage(imgStr, inc) {
-    var imgInc = document.createElement("img");
+var imgInc = document.createElement("img");
 
-    imgInc.src = (imgStr);
-    imgInc.alt = ('imgStr');
-    imgInc.width = 220/(1+(inc%3/3));
-    //imgInc.width depends on portrait or landscape
-    
-    $('<div id="item.w" data-i="'+(i+1)+'">')
-      .append(imgInc)
-      .appendTo('#thegrid');
-  }
+imgInc.src = (imgStr);
+imgInc.alt = ('imgStr');
+imgInc.width = 200/(1+(inc%3/3));
+//imgInc.width depends on portrait or landscape
+
+$('<div id="item.w" data-i="'+(i+1)+'">')
+.append(imgInc)
+.appendTo('#thegrid');
+}
 
 //called once when the page is refreshed from function() below
 function loadGridImages(numImages) { 
-  i=0;
-  console.log('Loading '+numImages+' images');
-  
-  for(i=0;i<numImages;i++){
-    var imgStr = "images/p"+(i+1)+".png";
-    addImage(imgStr, i);
-  }
-  //$('#thegrid').masonry();
+i=0;
+console.log('Loading '+numImages+' images');
 
-  imagesLoaded('#thegrid', function() {
-    $('#thegrid').masonry({
-      itemSelector: 'item',
-      columnWidth: ($(window).width())/50,
-      gutter: 12
-    });
+for(i=0;i<numImages;i++){
+var imgStr = "images/p"+(i+1)+".png";
+addImage(imgStr, i);
+}
+//$('#thegrid').masonry();
 
-  })
+imagesLoaded('#thegrid', function() {
+$('#thegrid').masonry({
+itemSelector: 'item',
+columnWidth: ($(window).width())/50,
+gutter: 12
+});
+
+})
 }
 
 function loadSlideshowImages(num) {
-  i=0;
-  console.log('Loading '+num+' slides');
-  
-  for(i=0;i<num;i++){
-    var imgInc = document.createElement("img");
-    imgInc.src = ("images/s/s_page_"+(i+1)+".png");
-    imgInc.alt = "images/s/s_page_"+(i+1)+".png";
+i=0;
+console.log('Loading '+num+' slides');
 
-    if (i < 9) {
-      imgInc.src = ("images/s/s_page_0"+(i+1)+".png");
-      imgInc.alt = "images/s/s_page_0"+(i+1)+".png";
-    }
-  else {
-      imgInc.src = ("images/s/s_page_"+(i+1)+".png");
-      imgInc.alt = "images/s/s_page_"+(i+1)+".png";
-    }
-  }
-  //$('#thegrid').masonry();
+for(i=0;i<num;i++){
+var imgInc = document.createElement("img");
+imgInc.src = ("images/s/s_page_"+(i+1)+".png");
+imgInc.alt = "images/s/s_page_"+(i+1)+".png";
 
-  imagesLoaded('#thegrid', function() {
-    console.log("Loaded Slides");
-    });
+if (i < 9) {
+imgInc.src = ("images/s/s_page_0"+(i+1)+".png");
+imgInc.alt = "images/s/s_page_0"+(i+1)+".png";
+}
+else {
+imgInc.src = ("images/s/s_page_"+(i+1)+".png");
+imgInc.alt = "images/s/s_page_"+(i+1)+".png";
+}
+}
+//$('#thegrid').masonry();
+
+imagesLoaded('#thegrid', function() {
+console.log("Loaded Slides");
+});
 }
 function loadBkgdImage() {
-  console.log('Loading Background Image');
-  showBackgroundImage(250);
+console.log('Loading Background Image');
+showBackgroundImage(250);
 
 }
 
 function removeImage(removeNum) {
-  i=0;
-  switch(removeNum){
-    case 0: location.reload();break;
-    case 1: $('#thegrid div[data-i=1]').remove();break;
-    case 2: $('#thegrid div[data-i=2]').remove();break;
-    case 3: $('#thegrid div[data-i=3]').remove();break;
-    case 4: 
-        //this might work if I change the filename to match the order and all intervals are the same
-        intervalID = setInterval(function() {
-          i++;
-          if(i%2 ==1) {
-            $('#thegrid div[data-i='+i+']').remove();
-          };
-          if(i>totalImages) {
-            clearInterval(intervalID);
-          };
-        }, 100);  break;
-    case 5:
-        //The full show, absolute times from button click
-        $("#bgimage").animate({opacity: 0}, 250);backgroundShowing=0;
-        $("#thegrid").animate({opacity: 1}, 1000);
-        setTimeout(function () {$('#thegrid div[data-i=1]').remove();},4000);
-        setTimeout(function () {$('#thegrid div[data-i=2]').remove();},8000);
-        setTimeout(function () {$('#thegrid div[data-i=3]').remove();},12000);
-        setTimeout(function () {$('#thegrid div[data-i=4]').remove();},16000);
-        setTimeout(function () {$('#thegrid div[data-i=5]').remove();},19000);
-        setTimeout(function () {$('#thegrid div[data-i=6]').remove();},23000);
-        setTimeout(function () {$('#thegrid div[data-i=7]').remove();},27000);
-        setTimeout(function () {$('#thegrid div[data-i=8]').remove();},31000);
-        setTimeout(function () {$('#thegrid div[data-i=9]').remove();},35000);
-        setTimeout(function () {$('#thegrid div[data-i=10]').remove();},39000);
-        setTimeout(function () {$('#thegrid div[data-i=11]').remove();},43000);
-        setTimeout(function () {$('#thegrid div[data-i=12]').remove();},47000);
-        setTimeout(function () {$('#thegrid div[data-i=13]').remove();},50000);
-        setTimeout(function () {$('#thegrid div[data-i=14]').remove();},55000);
-        setTimeout(function () {$('#thegrid div[data-i=15]').remove();},58000);
-        setTimeout(function () {$("#bgimage").animate({opacity: 1},12000);}, 50000);break;
-    case 6: 
-      console.lot("Button 6 pressed");break;
-    
-    //case 7: $('#thegrid div[data-i=7]').remove();break;
-    case 7: 
-      console.log("Previous Slide");
-      $('#slides img:last-child').remove();
-      showSlide((currentSlide>1)?--currentSlide:currentSlide);
-      break;
-    case 8: showSlideshow();break;
-    case 9: hideSlideshow();break;
-    case 10: 
-      console.log("Next slide");
-      $('#slides img:last-child').remove();
-      showSlide((currentSlide<totalSlides)?++currentSlide:currentSlide);
-      break;
-    //Note: transition remove setTimeout(function () {$('#thegrid' div[data-i=n]').animate({height:0, opacity: 0}, 3000);},absTime);
-  }
+i=0;
+switch(removeNum){
+case 0: location.reload();break;
+case 1: $('#thegrid div[data-i=1]').remove();break;
+case 2: $('#thegrid div[data-i=2]').remove();break;
+case 3: $('#thegrid div[data-i=3]').remove();break;
+case 4: 
+//this might work if I change the filename to match the order and all intervals are the same
+intervalID = setInterval(function() {
+  i++;
+  if(i%2 ==1) {
+    $('#thegrid div[data-i='+i+']').remove();
+  };
+  if(i>totalImages) {
+    clearInterval(intervalID);
+  };
+}, 100);  break;
+case 5:
+//The full show, absolute times from button click
+$("#bgimage").animate({opacity: 0}, 250);backgroundShowing=0;
+$("#thegrid").animate({opacity: 1}, 1000);
+setTimeout(function () {$('#thegrid div[data-i=1]').remove();},4000);
+setTimeout(function () {$('#thegrid div[data-i=2]').remove();},8000);
+setTimeout(function () {$('#thegrid div[data-i=3]').remove();},12000);
+setTimeout(function () {$('#thegrid div[data-i=4]').remove();},16000);
+setTimeout(function () {$('#thegrid div[data-i=5]').remove();},19000);
+setTimeout(function () {$('#thegrid div[data-i=6]').remove();},23000);
+setTimeout(function () {$('#thegrid div[data-i=7]').remove();},27000);
+setTimeout(function () {$('#thegrid div[data-i=8]').remove();},31000);
+setTimeout(function () {$('#thegrid div[data-i=9]').remove();},35000);
+setTimeout(function () {$('#thegrid div[data-i=10]').remove();},39000);
+setTimeout(function () {$('#thegrid div[data-i=11]').remove();},43000);
+setTimeout(function () {$('#thegrid div[data-i=12]').remove();},47000);
+setTimeout(function () {$('#thegrid div[data-i=13]').remove();},50000);
+setTimeout(function () {$('#thegrid div[data-i=14]').remove();},55000);
+setTimeout(function () {$('#thegrid div[data-i=15]').remove();},58000);
+setTimeout(function () {$("#bgimage").animate({opacity: 1},12000);}, 50000);break;
+case 6: 
+console.lot("Button 6 pressed");break;
 
-  //$('#thegrid div:last-child').remove();
+//case 7: $('#thegrid div[data-i=7]').remove();break;
+case 7: 
+console.log("Previous Slide");
+$('#slides img:last-child').remove();
+showSlide((currentSlide>1)?--currentSlide:currentSlide);
+break;
+case 8: showSlideshow();break;
+case 9: hideSlideshow();break;
+case 10: 
+console.log("Next slide");
+$('#slides img:last-child').remove();
+showSlide((currentSlide<totalSlides)?++currentSlide:currentSlide);
+break;
+//Note: transition remove setTimeout(function () {$('#thegrid' div[data-i=n]').animate({height:0, opacity: 0}, 3000);},absTime);
+}
+
+//$('#thegrid div:last-child').remove();
 }
 
 //smooth the resize to stop continuous triggers while resizing window
 function debouncer( func , timeout ) {
-   var timeoutID , timeout = timeout || 200;
-   return function () {
-      var scope = this , args = arguments;
-      clearTimeout( timeoutID );
-      timeoutID = setTimeout( function () {
-          func.apply( scope , Array.prototype.slice.call( args ) );
-      } , timeout );
-   }
+var timeoutID , timeout = timeout || 200;
+return function () {
+var scope = this , args = arguments;
+clearTimeout( timeoutID );
+timeoutID = setTimeout( function () {
+  func.apply( scope , Array.prototype.slice.call( args ) );
+} , timeout );
+}
 }
 
 function showBackgroundImage(fadeInTime) {
-  console.log("showBackgroundImage");
-  if (!backgroundShowing) {
-    $("#bgimage").ready(function(){showImage('/images/bkgd.png', "#bgimage")});
+console.log("showBackgroundImage");
+if (!backgroundShowing) {
+$("#bgimage").ready(function(){showImage('/images/bkgd.png', "#bgimage")});
     $("#bgimage").css({top: '10%'});
     $("#bgimage").delay(1*1000).animate({opacity: 1}, fadeInTime);
     backgroundShowing = 1;
@@ -229,7 +229,9 @@ socket.on('message', function(message){
 $(function() {
 
   //set height of content divs given the client's window
-  $(document).ready(function(){
+
+  //$(document).ready(function(){
+  $(window).load(function(){
     $('#bgwash').height($(window).height());
     $('#bgimage').height($(window).height());
     $('#container').height($(window).height());
